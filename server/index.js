f6fde7423cb4dd3e2bb0eafa2288fc0fa76c4c63
Christopher Wilson <cmw.wilson88@gmail.com>
2017-09-15 17:01:43 -0600

    change success redirect

--- server/index.js
+++ server/index.js
@@ -76,7 +76,7 @@ passport.use(new Auth0Strategy({
 }))
 
 app.get('/auth/', passport.authenticate('auth0'))
-app.get('/auth/callback/', passport.authenticate('auth0', {successRedirect: 'vnab.seewilsoncode.com/#/app/budget'}))
+app.get('/auth/callback/', passport.authenticate('auth0', {successRedirect: 'http://vnab.seewilsoncode.com/#/app/budget'}))
 
 passport.serializeUser(function(user, done) {
 	// let tempUserA = profileToSession
